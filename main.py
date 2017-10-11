71e596c5ba1d45c2f0f267c27c16a9a0a185181d
siyiwolf <392567441@qq.com>
2017-10-11 04:46:04 -0700

    Add example of data struct

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import python_p1 as myFun
+#!/usr/bin/env python
+#import python_p1 as myFun
 
-x = float(raw_input('Please input the dir:'));
-y = myFun.calcCircleValue(x);
-print("The circle Value is", y);
+#x = float(raw_input('Please input the dir:'));
+#y = myFun.calcCircleValue(x);
+#print "The circle Value is", y;
+
+i = 100;
+print i;
+i = 100.0;
+print i;
+str = 'I\'m ok!';
+print str;
