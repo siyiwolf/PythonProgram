28864316cf9830e2fb7d2c5613dc051f5c6da4a4
siyiwolf <392567441@qq.com>
2017-10-12 05:21:12 -0700

    The Study of data Struct!

--- main.py
+++ main.py
@@ -5,9 +5,29 @@
 #y = myFun.calcCircleValue(x);
 #print "The circle Value is", y;
 
-i = 100;
-print i;
-i = 100.0;
-print i;
-str = 'I\'m ok!';
-print str;
+#i = 100;
+#print i;
+#i = 100.0;
+#print i;
+#print 12 + 3.4
+#print 3.4 + 45
+#str = 'I\'m ok!';
+#print str;
+#str = 'Python World';
+#str = raw_input('Please input your name:');
+#welcome = 'Hello, %s' %(str);
+#print welcome;
+a = [1, 2, 'Two', 'what', 9];
+a.append('what');
+print a;
+print 'The second value of Array a %d is' %(a[1]);
+print 'The last second value of Array a %d is' %(a[-2]);
+a.insert(3, 'one');
+print 'After insert the num one at the index 3 %s' %a;
+c = ['cOne', 'cTwo', 'cThree'];
+a.insert(3, c);
+print 'After insert the self at the index 3 %s' %a;
+print a;
+b = a[3];
+print 'The four value of Array a %s is' %b;
+print b;
